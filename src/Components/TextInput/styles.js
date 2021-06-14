4232b6e1257bae3968c71e34a9399387c31be2f7
Thari zephaniah <56599367+Zeph-T@users.noreply.github.com>
2021-06-15 00:08:08 +0530

    Delete src directory

--- src/Components/TextInput/styles.js
+++
@@ -1,8 +0,0 @@
-import {StyleSheet} from 'react-native';
-import Colors from '../../Themes/Colors';
-import Metrics from '../../Themes/Metrics'
-
-export default StyleSheet.create({
-    input:{
-    }
-})
--- src/Themes/Colors.js
+++
@@ -1,9 +0,0 @@
-const Colors = {
-    backgroundColor : '#00ead3',
-    primaryColor : '#bb371a',
-    logoColor : '#0031FF',
-    HomePageGreetingCardColor : '#ce1212'
-    
-}
-
-export default Colors;
